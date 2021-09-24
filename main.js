23b7c023a4e765003efdb1dff932b37e6e18064c
moeenn <moeen.v8@gmail.com>
2021-09-24 18:26:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+import optimizeImage from "./lib/optimize.js";
+
+(() => main())()
+function main() {
+  const input = document.querySelector("#img-input");
+  input.addEventListener("change", async (event) => {
+    const file = event.target.files[0];
+    const encoding = await encodeBase64(file);
+
+    const optimizedEncoding = await optimizeImage(encoding);
+    previewEncoding(optimizedEncoding);
+  });
+}
+
+/**
+ *  convert a file into base64 encoding
+ * 
+*/
+function encodeBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      resolve(reader.result);
+    };
+    reader.onerror = function (error) {
+      reject('Error: ', error);
+    };
+  })
+}
+
+/**
+ *  preview base64 image: 
+ *  1. create image tag
+ *  2. set encoding as src
+ *  3. append image to document
+*/
+function previewEncoding(encoding, target = "#preview") {
+  const img = document.createElement("img");
+  img.src = encoding;
+
+  const targetElement = document.querySelector(target);
+  targetElement.innerHTML = '';
+  targetElement.appendChild(img);
+}
